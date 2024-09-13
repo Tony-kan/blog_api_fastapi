280404fe054d752bec5e17393d62fa5eeda89090
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-09-13 22:37:50 +0200

    added post routes and endpoinds

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import uvicorn
 from fastapi import FastAPI
 from database import Base, engine
 from core.user.routes import user_router
+from core.post.routes import post_router
 
 app = FastAPI()
 
 app.include_router(user_router, prefix="/users")
+app.include_router(post_router)
 
 Base.metadata.create_all(bind=engine)
 
